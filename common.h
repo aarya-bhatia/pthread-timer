2ed7e17df64669f1ae84c85b01d30fa263aa2594
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-02-25 09:24:56 -0600

    added imports

---
+++ common.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#define _GNU_SOURCE
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#include <pthread.h>
+#include <sys/time.h>
+#include <time.h>
+
+#ifdef DEBUG
+#include <assert.h>
+#include "log.h"
+#else
+#define assert(...) (void)0
+#define log_info(...) (void)0
+#define log_debug(...) (void)0
+#define log_warn(...) (void)0
+#define log_error(...) (void)0
+#endif
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
